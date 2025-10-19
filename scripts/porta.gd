4ce131963feac38efbe6c055317a3c2a726fad90
paulariett <Paula.Riett@edu.pucrs.br>
2025-10-19 17:29:18 -0300

    Modificando niveis e portas estao funcionais (menos pra voltar)

--- scripts/porta.gd
+++ scripts/porta.gd
@@ -1,16 +1,31 @@
 extends StaticBody2D
 
-@onready var game = get_parent().get_parent().get_parent()
+var game: Node2D
+var player: CharacterBody2D
+@onready var fechada = $Fechada
+@onready var aberta = $Aberta
+@onready var collider = $ColisaoPorta
+@onready var scene_limit = $SceneLimit
 
-func _physics_process(delta: float) -> void:
-	if game.level_cleared:
-		self.get_node("Fechada").hide()
-		self.get_node("Aberta").show()
-		self.get_node("CollisionShape2D").set_deferred("disabled", true)
-		#print("porta aberta")
+func _ready() -> void:
+	scene_limit.connect("body_entered", Callable(self, "_on_scene_limit_entered"))
+
+func _physics_process(delta):
+	if game and game.level_cleared:
+		abrir_porta()
 	else:
-		self.get_node("Fechada").show()
-		self.get_node("Aberta").hide()
-		self.get_node("CollisionShape2D").set_deferred("disabled", false)
-		#print("porta fechada")
-	
+		fechar_porta()
+
+func abrir_porta():
+	fechada.hide()
+	aberta.show()
+	collider.set_deferred("disabled", true)
+
+func fechar_porta():
+	fechada.show()
+	aberta.hide()
+	collider.set_deferred("disabled", false)
+
+func _on_scene_limit_entered(body):
+	if game and game.level_cleared:
+		game.mudar_para_proxima_fase()
