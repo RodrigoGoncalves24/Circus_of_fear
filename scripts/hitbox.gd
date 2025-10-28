306ededfc18030434147c37992aff391b06a5a03
Thomaz <141247521+thomazabrantes@users.noreply.github.com>
2025-10-27 23:56:49 -0300

    3 vidas para zumbis

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -3,11 +3,18 @@ extends Area2D
 @onready var damage = $"../damage"
 @onready var hurt_timer = $"../hurt_timer"
 
+var vida = 3
+
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("bullets"):
 		area.queue_free()
+		
+		vida -= 1
+		
 		damage.play("damage_to_zombie")
 		hurt_timer.start()
 		await hurt_timer.timeout
 		damage.play("RESET")
-		owner.queue_free()
+		
+		if vida <= 0:
+			owner.queue_free()
