4e7b16e30e14c7cdcf80a0c2be56e28c680a977b
Thomaz <141247521+thomazabrantes@users.noreply.github.com>
2025-10-27 23:50:38 -0300

    efeito de dano nos zumbis

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -1,6 +1,13 @@
 extends Area2D
 
+@onready var damage = $"../damage"
+@onready var hurt_timer = $"../hurt_timer"
+
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("bullets"):
 		area.queue_free()
+		damage.play("damage_to_zombie")
+		hurt_timer.start()
+		await hurt_timer.timeout
+		damage.play("RESET")
 		owner.queue_free()
