6116183cd689ec745fc8355d0421da6ca32cac96
Paula Armiliato Riett <Paula.Riett@edu.pucrs.br>
2025-10-31 18:41:57 -0300

    Testes de porta e ajustes de funcao de game.gd

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,23 +1,11 @@
 extends Node2D
 
-@onready var player: CharacterBody2D
-@onready var level: Node
+@onready var level: Node = $Level
+@onready var player: CharacterBody2D = level.get_node("Crianca")
 
-@onready var level_cleared: bool = false
 @onready var current_level = 1
 
-func  _ready() -> void:
-	Input.set_custom_mouse_cursor(load("res://textures/mira.png"))
-	var qtd_filhos = get_child_count()
-	# Assumindo que o level é SEMPRE o último nodo da cena!
-	level = get_child(qtd_filhos-1)
-	player = level.get_node("Crianca")
-	for porta in get_tree().get_nodes_in_group("portas"):
-		porta.game = self
-			
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("level_cleared"):
-		switch_level_status(!level_cleared)
+@export var monsters_count = 2
 	
 func proxima_fase():
 	call_deferred("goto_scene", "res://scenes/level_" + str(switch_level(1)) + ".tscn")
@@ -27,14 +15,10 @@ func fase_anterior():
 	call_deferred("goto_scene", "res://scenes/level_" + str(switch_level(-1)) + ".tscn")
 	print("Level: " + str(current_level))
 	
-func switch_level_status(new_status: bool):
-	level_cleared = new_status
-	print("Cleared: " + str(level_cleared))
-	
 func switch_level(qtd: int) -> int:
 	# Aumenta ou diminui o contador de fases e reseta a flag de fase finalizada
-	if qtd > 0:
-		level_cleared = false
+	#if qtd > 0:
+		#level_cleared = false
 	current_level += qtd
 	return current_level
 
@@ -46,5 +30,3 @@ func goto_scene(path: String):
 	var new_scene : PackedScene = ResourceLoader.load(path)
 	level = new_scene.instantiate()
 	get_tree().get_root().get_child(0).add_child(level)
-	for porta in get_tree().get_nodes_in_group("portas"):
-		porta.game = self
--- scripts/porta.gd
+++ scripts/porta.gd
@@ -1,24 +1,33 @@
 extends StaticBody2D
 
-var game: Node2D
-var player: CharacterBody2D
+@onready var game: Node2D = get_tree().root.get_node("Game")
+@onready var player: CharacterBody2D
 @onready var fechada = $Fechada
 @onready var aberta = $Aberta
 @onready var collider = $ColisaoPorta
 @onready var scene_limit = $SceneLimit
-@onready var scene_limit_prev
+var scene_limit_prev
+
+@onready var level_cleared: bool = false
 
 func _ready() -> void:
 	scene_limit.connect("body_entered", Callable(self, "_on_scene_limit_entered"))
 	if $SceneLimitPrev:
 		scene_limit_prev = $SceneLimitPrev
 		scene_limit_prev.connect("body_entered", Callable(self, "_on_scene_limit_prev_entered"))
-
-func _physics_process(delta):
-	if game and game.level_cleared:
+		
+func _physics_process(delta: float) -> void:
+	#if monsters_count == 0:
+	if Input.is_action_just_pressed("level_cleared"):
+		toggle_level_status()
+	
+func toggle_level_status():
+	level_cleared = not level_cleared
+	if level_cleared:
 		abrir_porta()
 	else:
 		fechar_porta()
+	print("Level cleared: " + str(level_cleared))
 
 func abrir_porta():
 	fechada.hide()
@@ -31,9 +40,10 @@ func fechar_porta():
 	collider.set_deferred("disabled", false)
 
 func _on_scene_limit_entered(body):
-	if game and game.level_cleared:
+	if game and level_cleared:
 		game.proxima_fase()
 
 func _on_scene_limit_prev_entered(body):
+	print("vai voltar")
 	if game:
 		game.fase_anterior()
