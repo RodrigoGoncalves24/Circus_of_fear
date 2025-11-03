aaf8cb6ce10b7c49e284fe8d81c3eb6672c138f9
paulariett <Paula.Riett@edu.pucrs.br>
2025-11-03 16:39:14 -0300

    Adicionando tela de pause (bugada ainda) e tela de controles melhorada

---
+++ scripts/controles.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@onready var sequence = [
+	$ColorRect/w, 
+	$ColorRect/a, 
+	$ColorRect/s, 
+	$ColorRect/d,
+	$ColorRect/e, 
+	$ColorRect/esc, 
+	$ColorRect/mouse
+	]
+var current_index := 0
+var timer: Timer
+
+func _ready():
+	timer = Timer.new()
+	timer.wait_time = 0.5
+	timer.autostart = true
+	timer.one_shot = false
+	add_child(timer)
+	timer.timeout.connect(_on_timer_timeout)
+
+func _on_timer_timeout():
+	var key = sequence[current_index]
+	_highlight_key(key)
+	current_index = (current_index + 1) % sequence.size()
+
+func _highlight_key(key):
+	key.play("default")
+	await get_tree().create_timer(1.0).timeout
+	key.stop()
--- scripts/game.gd
+++ scripts/game.gd
@@ -2,11 +2,31 @@ extends Node2D
 
 @onready var level: Node = $Level
 @onready var player: CharacterBody2D = level.get_node("Crianca")
+@onready var pause: Control = $Game_Paused
 
 @onready var current_level = 1
 
 @export var monsters_count = 2
+
+func _ready() -> void:
+	pause.hide()
 	
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause") and get_tree().paused:
+		print("UNPAUSE")
+		_Unpause()
+	elif Input.is_action_just_pressed("pause"):
+		print("PAUSE")
+		_Pause()
+	
+func _Pause():
+	get_tree().paused = true
+	pause.show()
+	
+func _Unpause():
+	get_tree().paused = false
+	pause.hide()
+		
 func proxima_fase():
 	call_deferred("goto_scene", "res://scenes/level_" + str(switch_level(1)) + ".tscn")
 	print("Level: " + str(current_level))
@@ -16,9 +36,6 @@ func fase_anterior():
 	print("Level: " + str(current_level))
 	
 func switch_level(qtd: int) -> int:
-	# Aumenta ou diminui o contador de fases e reseta a flag de fase finalizada
-	#if qtd > 0:
-		#level_cleared = false
 	current_level += qtd
 	return current_level
 
@@ -30,3 +47,4 @@ func goto_scene(path: String):
 	var new_scene : PackedScene = ResourceLoader.load(path)
 	level = new_scene.instantiate()
 	get_tree().get_root().get_child(0).add_child(level)
+	
